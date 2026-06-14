c59a5aa16970358d12c669928202312e7bc73889
kamedia140-beep <kamedia140@gmail.com>
2026-06-14 10:45:40 +0200

    Update bot settings: change bot name, owner name, and owner number

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'sᴜᴘʀᴇᴍᴇ ᴀʟɪ',
+  author: '𝗞.𝗔 ᴛᴇᴄʜ',
+  botName: "sᴜᴘʀᴇᴍᴇ ᴀʟɪ",
+  botOwner: '𝗞.𝗔 ᴛᴇᴄʜ', // Your name
+  ownerNumber: '263781312500', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
